4221b5df1ba72a73fb013a5986589e0c5c61d0ac
Varun Kumar <varunkumaranalysis@gmail.com>
2019-10-02 12:26:16 -0400

    done with basics

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -4,17 +4,22 @@ import BotCard from "../components/BotCard";
 class BotCollection extends React.Component {
   //your code here
 
-  render(){
-  	return (
-  	  <div className="ui four column grid">
-    		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
-    		</div>
-  	  </div>
-  	);
+  render() {
+    return (
+      <div className="ui four column grid">
+        <div className="row">
+          {/* I want to map through my array of bots, and for each one, render a
+          BotCard */}
+          {this.props.bots.map(bot => {
+            return (
+              <BotCard bot={bot} handleCardClick={this.props.handleCardClick} />
+            );
+          })}
+          {/* {console.log(this.props.bots)} Collection of all bots */}
+        </div>
+      </div>
+    );
   }
-
-};
+}
 
 export default BotCollection;
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,16 +1,52 @@
 import React from "react";
+import BotCollection from "./BotCollection";
+import YourBotArmy from "./YourBotArmy";
 
 class BotsPage extends React.Component {
   //start here with your code for step one
+  state = {
+    bots: [],
+    cardClicked: false,
+    botArmy: []
+  };
 
+  componentDidMount() {
+    fetch("https://bot-battler-api.herokuapp.com/api/v1/bots")
+      .then(resp => resp.json())
+      .then(bots =>
+        this.setState({
+          bots: bots
+        })
+      );
+    // console.log(this.state.botArmy.push("bot"));
+  }
+
+  handleCardClick = (e, id) => {
+    // console.log("clicked!");
+    let bot = this.state.bots.find(bot => bot.id === id);
+    let botArmycopy = [...this.state.botArmy];
+    botArmycopy.push(bot);
+    this.setState({
+      cardClicked: true,
+      botArmy: botArmycopy
+    });
+
+    console.log(bot);
+  };
   render() {
     return (
       <div>
-        {/* put your components here */}
+        <YourBotArmy
+          cardClicked={this.state.cardClicked}
+          botArmy={this.state.botArmy}
+        />
+        <BotCollection
+          bots={this.state.bots}
+          handleCardClick={this.handleCardClick}
+        />
       </div>
     );
   }
-
 }
 
 export default BotsPage;
